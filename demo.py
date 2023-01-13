680e145da7b8c89653ba2a6dd10eb9955551cd92
Hemanth <nalamhemanth1999@gmail.com>
2023-01-13 12:00:11 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,13 +1,7 @@
 print("Hello iNeuron!")
 print("Hello")
 print("Hello iNeuron!")
-<<<<<<< HEAD
-print("Hel0")
-||||||| 3a47d34
-print("It's line 4.0")
-=======
 print("It's line 4.")
->>>>>>> fe207577b3effcb5b15ded9711ff3da1dfe964d3
 print("Hello iNeuron!")
 print("Hello")
 print("Hello iNeuron!")
